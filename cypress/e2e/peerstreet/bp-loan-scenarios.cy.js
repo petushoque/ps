c40bf764bbeb34c9171069ee5db57ca042c67b1b
Kubasov Alexei <kubasovalexei@yandex.ru>
2022-08-02 15:59:11 +0700

    Update bp-loan-scenarios.cy.js

--- cypress/e2e/peerstreet/bp-loan-scenarios.cy.js
+++ cypress/e2e/peerstreet/bp-loan-scenarios.cy.js
@@ -12,7 +12,7 @@ describe('Regression test for creating loan scenarios', () => {
         return false;
     });
 
-    //добавить изменение очком и спреда
+    //добавить изменение очков и спреда
     it('should update the rate in the Peersuite Settings', () => {
         cy.visit(`${lenderPortalStaging}/users/sign_in`, { failOnStatusCode: false });
         cy.get('#user_email').type(lender.email);
@@ -21,31 +21,22 @@ describe('Regression test for creating loan scenarios', () => {
         cy.log('the lender is authorized');
 
         cy.visit(`${lenderPortalStaging}/peersuite_settings/rates`)
-        cy.get('input[name="program[residential_bridge][show_rates]"]', { timeout: 15000 }).first().click();
-        cy.log('the display of rates for Residential Bridge is set');
-        cy.get('input[name="program[residential_rental_long_term][show_rates]"]', { timeout: 15000 }).first().click()
-        cy.log('the display of rates for Residential for Rent is set');
-        cy.get('button[type="button"]').click({ multiple: true, force: true });
-        cy.log('settings saved');
+        cy.selectRates('price')
     })
 
     it('should display rates for the borrower (Residential Bridge)', () => {
         cy.visit(`${borrowerPortalStaging}/borrowers/loan-scenarios`);
-
         cy.createResidentialBridgeLoan();
-
         cy.get('tr', { timeout: 15000 }).its('length').should('be.gt', 1)
     })
 
     it('should display rates for the borrower (Residential for Rent)', () => {
         cy.visit(`${borrowerPortalStaging}/borrowers/loan-scenarios`);
-
         cy.createResidentialForRentLoan();
-
         cy.get('tr', { timeout: 15000 }).its('length').should('be.gt', 1)
     })
 
-    //добавить текста для Request Recieved Screen
+    //добавить изменение текста для Request Recieved Screen
     it('should update the Request Recieved Screen in the Peersuite Settings', () => {
         cy.visit(`${lenderPortalStaging}/users/sign_in`, { failOnStatusCode: false });
         cy.get('#user_email').type(lender.email);
@@ -53,13 +44,8 @@ describe('Regression test for creating loan scenarios', () => {
         cy.get('input[name="commit"]').click();
         cy.log('the lender is authorized');
 
-        cy.visit(`${lenderPortalStaging}/peersuite_settings/rates`)
-        cy.get('input[name="program[residential_bridge][show_rates]"]', { timeout: 15000 }).last().click();
-        cy.log('the display of rates for Residential Bridge is set');
-        cy.get('input[name="program[residential_rental_long_term][show_rates]"]', { timeout: 15000 }).last().click()
-        cy.log('the display of rates for Residential for Rent is set');
-        cy.get('button[type="button"]').click({ multiple: true, force: true });
-        cy.log('settings saved');
+        cy.visit(`${lenderPortalStaging}/peersuite_settings/rates`);
+        cy.selectRates('request-recieved-screen');
     })
 
 })
